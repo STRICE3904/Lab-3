1712a8ae4e4079da6722d719d0d70ef3507800f7
STRICE3904 <trice4@tritons.iowacentral.edu>
2021-09-21 10:52:32 -0500

    Ok its fine now

--- Lab-3/Source.cpp
+++ Lab-3/Source.cpp
@@ -1,6 +1,6 @@
 /*Samuel Trice
 * C++ 2021
-* September 15th
+* September 29th
 * Lab 3
 * Computation of mean and standard deviation of a poputlation set by file "inMeanStd.dat" and
 * with an outpute called "outMeanStd.dat
@@ -14,7 +14,7 @@ using namespace std;
 void mathStandard();
 void mathMean();
 
-//These are the variables being ussed for all functions that are inputed by the user
+//These are the variables being ussed for all functions that are inputed by the file
 int data1;
 int data2;
 int data3;
@@ -28,15 +28,19 @@ int main()
 	string outputFileName = "C:/Users/quinn/source/Lab-3";
 	ifstream infile;
 	ofstream outfile;
+
 	//This is the start of the file prosses
 	infile.open("inMeanStd.dat");
 	outfile.open("outMeanStd.dat");
-	//getline(inFile, data);
+
+	//file being transported into variables then exported to the screan to check it works. 
 	infile >> data1 >> data2 >> data3 >> data4;
 	cout << "Data from inMeanStd.dat = ";
 	cout << data1 << " " << data2 << " " << data3 << " " << data4 << endl;
 
+	//Start of ussing the variables to find the standard deviation of the 4 integers. 
 
+	//Returning the new values back into the file outMeanStd.dat
 
 	infile.close();
 	outfile.close();
@@ -45,12 +49,15 @@ int main()
 }
 void mathMean()
 {
+	//The values are added together for the first step of calculating the mean
 	float Add;
 	Add = float(data1) + float(data2) + float(data3) + float(data4);
-	//The values are added together for the first step of calculating the mean
+
+	//The mean is calculated 
 	float Mean;
 	Mean = double(Add / 4);
-	//The mean is calculated 
+
+	//The mean is then outputed to teh screan
 	cout << "The mean of the inputed values is: " << Mean << endl;
 }
 void mathStandard()
@@ -60,17 +67,18 @@ void mathStandard()
 	*/
 	float Add;
 	Add = float(data1) + float(data2) + float(data3) + float(data4);
-	//The values are added together for the first step of calculating the mean
 	float Mean;
 	Mean = double(Add / 4);
-	//The mean is calculated 
+	
 	//Each part of the sum for the vareince is calculated seperately to minimize errors. 
 	float VD1 = float(data1) - float(Mean);
 	float VD2 = float(data2) - float(Mean);
 	float VD3 = float(data3) - float(Mean);
 	float VD4 = float(data4) - float(Mean);
+
 	//The population vareince is calculated here
 	float Varience = float(pow(VD1, 2) + pow(VD2, 2) + pow(VD3, 2) + pow(VD4, 2)) / float(4);
+
 	//The Standard deviation is calculated from the vareince here
 	float StDv = float(sqrt(Varience));
 	cout << "Standard Deviation of the inputed values = " << StDv << endl;
